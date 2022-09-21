afacdde76ac2d792ab90d94ad669aa9e282f89f3
AnnaYeremenko <a.zaliubovska@gmail.com>
2022-09-21 19:04:55 +0300

    hw 06

--- src/js/task-04.js
+++
@@ -1,9 +0,0 @@
-let counterValue = 0
-const btnEl = document.querySelectorAll("button");
-const valueEl = document.querySelector("#value");
-btnEl[0].addEventListener("click", () => {
-  valueEl.textContent = counterValue -= 1
-})
-btnEl[1].addEventListener("click", () => {
-  valueEl.textContent = counterValue += 1
-})
