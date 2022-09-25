5e9fd7e4ce83ec792ae22c4aaeb81e0b2c7e6b39
AnnaYeremenko <a.zaliubovska@gmail.com>
2022-09-26 00:15:02 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -11,3 +11,28 @@ inputRef.addEventListener('blur', () => {
         return inputRef.classList.add('invalid');
     }
 });
+
+// const validInput = document.querySelector("#validation-input");
+// const lengthInput = Number(validInput.dataset.length);
+// validInput.addEventListener("blur", onBlur);
+// function onBlur() {
+//     validInput.classList.remove("invalid", "valid");
+//     const classRt = validInput.value.length === lengthInput ? "valid" : "invalid"
+//     validInput.classList.add(classRt);
+// };
+// // Варіант №2
+// const validInput = document.querySelector("#validation-input");
+// const lengthInput = Number(validInput.dataset.length);
+// validInput.addEventListener("blur", onBlur);
+// function onBlur() {
+//     if (validInput.value.length === lengthInput) {
+//         validInput.classList.remove("invalid");
+//         validInput.classList.add("valid");
+//     } else {
+//         validInput.classList.remove("valid");
+//         validInput.classList.add("invalid");
+//     }
+//     if (validInput.value.length === 0) {
+//         validInput.classList.remove("invalid");
+//     }
+// };
